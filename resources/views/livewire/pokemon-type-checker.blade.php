d29452934a5ae588f495c8606b54fbeea7229815
Mikey Ying <mikeyinguk@gmail.com>
2023-09-24 11:31:51 +0100

    styling

--- resources/views/livewire/pokemon-type-checker.blade.php
+++ resources/views/livewire/pokemon-type-checker.blade.php
@@ -2,20 +2,24 @@
     <div class='p-4 bg-indigo-700'>
         <h1>Pokémon Type Checker</h1>
         <div class='my-4 p-2 bg-indigo-400 text-slate-900'>
-            <label for='attackingType'>Attacking Type:</label>
-            <input
-                wire:model='attackingType'
-                type='text'
-                id='attackingType'
-            >
-        </div>
-        <div class='my-4 p-2 bg-indigo-400 text-slate-900'>
-            <label for='defendingType'>Defending Type:</label>
-            <input
-                wire:model='defendingType'
-                type='text'
-                id='defendingType'
-            >
+            <div class='p-2 flex justify-between'>
+                <label for='attackingType'>Attacking Type:</label>
+                <input
+                    class='ml-2 rounded'
+                    wire:model='attackingType'
+                    type='text'
+                    id='attackingType'
+                >
+            </div>
+            <div class='p-2 flex justify-between'>
+                <label for='defendingType'>Defending Type:</label>
+                <input
+                    class='ml-2 rounded'
+                    wire:model='defendingType'
+                    type='text'
+                    id='defendingType'
+                >
+            </div>
         </div>
         <button
             class='border rounded p-2 border-slate-200'

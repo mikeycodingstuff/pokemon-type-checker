fefffe7ecd6584553cae7048de67d3f04a13d2f7
Mikey Ying <mikeyinguk@gmail.com>
2023-09-24 19:27:14 +0100

    accessor for effectiveness

--- app/Models/TypeEffectiveness.php
+++ app/Models/TypeEffectiveness.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,6 +11,20 @@ class TypeEffectiveness extends Model
     use HasFactory;
 
     protected $table = 'type_effectiveness';
+
+    protected function effectiveness(): Attribute
+    {
+        return Attribute::make(
+            get: function (string $value) {
+                return match ($value) {
+                    '2.0' => 'super effective',
+                    '1.0' => 'normal effectiveness',
+                    '0.5' => 'not very effective',
+                    '0.0' => 'no effect',
+                };
+            }
+        );
+    }
 
     public function attackingType()
     {
